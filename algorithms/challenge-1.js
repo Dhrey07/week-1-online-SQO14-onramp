80f7232ef417bcf5384deeac6574c6c93582592d
Dhrey07 <oyebamijidamilare71@gmail.com>
2022-11-02 00:09:31 +0100

    done

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,10 @@
 function countTruthy(arr) {
-
+    let count = 0;
+for(let i = 0; i<arr.length; i++){
+    if(typeof(arr[i])==='number' && arr[i] >0){
+        count++
+    }
+}
+ return count
 }
 module.exports = countTruthy
--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,25 @@
 function calculateGrade(marks) {
+    let F = 1 >= 49;
+    let E = 50 >= 59;
+    let D = 60 >= 69;
+    let C = 70 >= 79;
+    let B = 80 >= 89;
+    let A = 90 >= 100;
+
+    
+    if(marks / marks.length <= 49){
+        return F;
+    }else if(marks / marks.length <= 59 ){
+        return E;
+    }else if(marks / marks.length <= 69 ){
+        return D;
+    }else if(marks / marks.length <= 79 ){
+        return C;
+    }else if(marks / marks.length <= 89){
+        return B;
+    }else if(marks / marks.length <= 100){
+        return A;
+    }
  
  }
   module.exports =calculateGrade
